3a999dd08ce972dcd55f7dd5273a6a303673011f
pikaia <chriskslee@gmail.com>
2016-08-12 01:20:19 -0400

    Add more permutations of name.

--- content_script.js
+++ content_script.js
@@ -1,39 +1,35 @@
-function walk(node)
-{
+function walk(node) {
 	var child, next;
-	
-	switch ( node.nodeType )
-	{
+
+	switch (node.nodeType) {
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child )
-			{
+			while (child) {
 				next = child.nextSibling;
 				walk(child);
 				child = next;
 			}
 			break;
-		
+
 		case 3: // Text node
 			handleText(node);
 			break;
 	}
 }
 
-function handleText(textNode)
-{
+function handleText(textNode) {
 	var v = textNode.nodeValue;
-	
-    v = v.replace(/\bDonald J. Trump\b/g, "The Faker").replace(/\bDonald Trump\b/g, "The Faker").replace(/\bTrump\b/g, "Faker").replace(/\bDonald\b/g, "The Faker");
-	
+
+    v = v.replace(/\bDonald Trumps\b/g, "Fakers").replace(/\bDonald J. Trump\b/g, "The Faker").replace(/\bDonald Trump\b/g, "The Faker").replace(/\bTrump\b/g, "Faker").replace(/\bDonald\b/g, "The Faker");
+
 	textNode.nodeValue = v;
 }
 
 walk(document.body);
 
-new MutationObserver(function() {
+new MutationObserver(function () {
 	walk(document.body);
 }).observe(document.body, {
 	childList: true
